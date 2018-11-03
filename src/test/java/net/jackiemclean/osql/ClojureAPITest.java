f8ddb0a3021b2ca9bbfba151ea2559f0292f5006
mikey <mikey@mikeyaustin.com>
2018-11-03 22:20:58 +0100

    added clojure test

--- src/test/java/net/jackiemclean/osql/ClojureAPITest.java
+++ src/test/java/net/jackiemclean/osql/ClojureAPITest.java
@@ -2,15 +2,19 @@ package net.jackiemclean.osql;
 
 import static org.junit.Assert.*;
 
+import clojure.java.api.Clojure;
+import clojure.lang.IFn;
 import java.util.UUID;
 import net.jackiemclean.osql.OrderProto.Order;
 import org.junit.Test;
 
-public class OrderProtoTest {
+public class ClojureAPITest {
   @Test
-  public void testOrderBuilder() {
+  public void testSimple() {
     UUID orderId = UUID.randomUUID();
     Order order = Order.newBuilder().setId(orderId.toString()).build();
-    assertEquals(order.getId(), orderId.toString());
+
+    IFn plus = Clojure.var("clojure.core", "+");
+    assertEquals(3L, plus.invoke(1, 2));
   }
 }
